4bee39a8868f22e0d2b2de0cdc6323d9176193fe
zTzyrant <61049884+zTzyrant@users.noreply.github.com>
2023-12-02 05:43:33 +0800

    fix: file upload functionality and handle form data

--- functions/picture_upload.js
+++ functions/picture_upload.js
@@ -1,9 +1,17 @@
 const formidable = require("formidable")
 const mv = require("mv")
 const path = require("path")
+const fs = require("fs-extra")
 
+// handle form data from request
 const handleFormData = async (req) => {
-  const form_data = new formidable.IncomingForm()
+  // remove all files in temp/uploads
+  await fs.emptyDir(path.join(__dirname, "../temp/uploads"))
+  const form_data = new formidable.IncomingForm({
+    multiples: true,
+    uploadDir: path.join(__dirname, "../temp/uploads"),
+    keepExtensions: true,
+  })
   const [fields, files] = await new Promise((resolve, reject) => {
     form_data.parse(req, (err, fields, files) => {
       if (err) reject(err)
@@ -13,6 +21,7 @@ const handleFormData = async (req) => {
   return { fields, files }
 }
 
+// upload picture to uploads folder
 const uploadPicture = async (files) => {
   const pictures = []
   try {
@@ -31,7 +40,7 @@ const uploadPicture = async (files) => {
     })
     return pictures
   } catch (error) {
-    console.log(error)
+    console.log("Error upload picture: ", error)
     return null
   }
 }
